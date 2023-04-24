4fb3bf3ad18f8ee65fac50e09b7cf152dad7fc49
magneticant <milance.za019@gmail.com>
2023-04-24 15:48:08 +0200

    Inicijalni commit za projekat Server.

---
+++ storage-manager-server/src/main/java/rs/np/storage_manager_server/App.java
@@ -0,0 +1,13 @@
+package rs.np.storage_manager_server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
